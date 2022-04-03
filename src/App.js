13d468eba409bfed61cd396cfeab9c3d94bf0caf
Bunny Mueller <thebunnymueller@gmail.com>
2022-04-03 13:37:12 +0200

    Routing Practice Assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import {Route, Routes, useParams} from 'react-router';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+
+const Home = (params) => {
+  return (<h1>WELCOME!</h1>);
+}
+
+const SimplePathArgument = (params) => {
+  const {arg} = useParams();
+  if (isNaN(arg)) {
+    return (<h1>{arg}</h1>);
+  } else {
+    return (<h1>Number: {arg}</h1>);
+  }
+}
+
+const FancyWord = (params) => {
+  const {word, fontcolour, backcolour} = useParams();
+  return (<h1 style={{color: fontcolour, background: backcolour}}>{word}</h1>);
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/:arg" element={<SimplePathArgument />} />
+        <Route path="/:word/:fontcolour/:backcolour" element={<FancyWord/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
